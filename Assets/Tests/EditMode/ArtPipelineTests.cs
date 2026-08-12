79b2e5480abb3cbb335f29b2488f08cef6dac136
liaohongwei <liaohongwei@ssish.com>
2026-08-13 00:31:45 +0800

    feat: integrate validated pixel art pipeline

--- Assets/Tests/EditMode/ArtPipelineTests.cs
+++ Assets/Tests/EditMode/ArtPipelineTests.cs
@@ -1,12 +1,22 @@
 using System;
+using System.Linq;
 using NUnit.Framework;
+using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using YuanHaiLu.Art;
+using YuanHaiLu.Core;
+using YuanHaiLu.Editor;
 
 namespace YuanHaiLu.Tests.EditMode
 {
     public class ArtPipelineTests
     {
+        private const string TestCharacterPath =
+            "Assets/Art/Characters/Player/player_male_swordsman.png";
+        private const string TestTilesetPath =
+            "Assets/Art/Environment/Regions/yanliu/yanliu_tileset.png";
+
         private CharacterArtCatalog characterCatalog;
         private EnvironmentArtCatalog environmentCatalog;
 
@@ -94,6 +104,114 @@ namespace YuanHaiLu.Tests.EditMode
             Assert.That(ArtAssetId.IsValid("boss_cangyue_01"), Is.True);
             Assert.That(ArtAssetId.IsValid("Boss-CangYue"), Is.False);
             Assert.That(ArtAssetId.IsValid(string.Empty), Is.False);
+        }
+
+        [Test]
+        public void FormalCharacterTextureUsesExactPixelImportSettings()
+        {
+            ArtImportRules.Apply(TestCharacterPath);
+            var importer = (TextureImporter)AssetImporter.GetAtPath(TestCharacterPath);
+
+            Assert.That(importer.spritePixelsPerUnit, Is.EqualTo(16f));
+            Assert.That(importer.filterMode, Is.EqualTo(FilterMode.Point));
+            Assert.That(importer.mipmapEnabled, Is.False);
+            Assert.That(importer.textureCompression, Is.EqualTo(TextureImporterCompression.Uncompressed));
+            Assert.That(importer.spriteImportMode, Is.EqualTo(SpriteImportMode.Multiple));
+            Assert.That(
+                AssetDatabase.LoadAllAssetsAtPath(TestCharacterPath).OfType<Sprite>().Count(),
+                Is.EqualTo(244));
+        }
+
+        [Test]
+        public void FormalTilesetUsesMetadataSpriteNames()
+        {
+            ArtImportRules.Apply(TestTilesetPath);
+            var names = AssetDatabase.LoadAllAssetsAtPath(TestTilesetPath)
+                .OfType<Sprite>()
+                .Select(sprite => sprite.name)
+                .ToArray();
+
+            Assert.That(names, Does.Contain("yanliu__grass__0"));
+            Assert.That(names, Does.Contain("yanliu__water_deep__0"));
+        }
+
+        [Test]
+        public void FormalArtValidatorReportsNoReferenceSliceErrors()
+        {
+            ArtImportRules.ApplyAllFormal();
+            var report = ArtAssetValidator.ValidateAll();
+
+            Assert.That(report.Errors, Is.Empty, report.ToString());
+        }
+
+        [Test]
+        public void FormalBatchImportLeavesUndeclaredLegacyTexturesUntouched()
+        {
+            const string legacyPath = "Assets/Art/Tilesets/yanliu_town_demo.png";
+            var importer = (TextureImporter)AssetImporter.GetAtPath(legacyPath);
+            var before = new
+            {
+                importer.textureType,
+                importer.spriteImportMode,
+                importer.filterMode,
+                importer.mipmapEnabled,
+                importer.textureCompression
+            };
+
+            ArtImportRules.ApplyAllFormal();
+            importer = (TextureImporter)AssetImporter.GetAtPath(legacyPath);
+
+            Assert.That(importer.textureType, Is.EqualTo(before.textureType));
+            Assert.That(importer.spriteImportMode, Is.EqualTo(before.spriteImportMode));
+            Assert.That(importer.filterMode, Is.EqualTo(before.filterMode));
+            Assert.That(importer.mipmapEnabled, Is.EqualTo(before.mipmapEnabled));
+            Assert.That(importer.textureCompression, Is.EqualTo(before.textureCompression));
+        }
+
+        [Test]
+        public void GeneratedDefaultCatalogsResolveTheReferenceSlice()
+        {
+            var characters = AssetDatabase.LoadAssetAtPath<CharacterArtCatalog>(
+                ArtCatalogBuilder.CharacterCatalogPath);
+            var environments = AssetDatabase.LoadAssetAtPath<EnvironmentArtCatalog>(
+                ArtCatalogBuilder.EnvironmentCatalogPath);
+
+            Assert.That(characters, Is.Not.Null);
+            Assert.That(environments, Is.Not.Null);
+            Assert.That(characters.TryGet("player_male_swordsman", out var male), Is.True);
+            Assert.That(male.Sheet, Is.Not.Null);
+            Assert.That(environments.TryGet("yanliu", out var yanliu), Is.True);
+            Assert.That(yanliu.Landmarks, Is.Not.Null);
+            Assert.That(yanliu.Preview, Is.Not.Null);
+        }
+
+        [Test]
+        public void ArtReferenceSceneContainsOnlyPersistentSpriteAssets()
+        {
+            var scene = EditorSceneManager.OpenScene(
+                ArtReferencePreviewGenerator.ScenePath,
+                OpenSceneMode.Additive);
+            try
+            {
+                var roots = scene.GetRootGameObjects();
+                Assert.That(roots.Select(root => root.name), Does.Contain("ReferenceCharacters"));
+                Assert.That(roots.Select(root => root.name), Does.Contain("YanliuEnvironment"));
+                Assert.That(
+                    roots.SelectMany(root => root.GetComponentsInChildren<GameManager>(true)),
+                    Is.Empty);
+                var renderers = roots.SelectMany(
+                    root => root.GetComponentsInChildren<SpriteRenderer>(true)).ToArray();
+                Assert.That(renderers.Length, Is.EqualTo(25));
+                Assert.That(renderers.All(renderer => renderer.sprite != null), Is.True);
+                Assert.That(
+                    renderers.All(renderer => AssetDatabase.Contains(renderer.sprite)),
+                    Is.True,
+                    "Reference scene must not contain runtime-created placeholder sprites.");
+            }
+            finally
+            {
+                EditorSceneManager.CloseScene(scene, true);
+            }
         }
     }
 }
